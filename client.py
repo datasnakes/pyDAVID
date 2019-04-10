7f8ff539a46c4e31ad1cf00d704e83dc6a7aa5d1
S. Hutchins <ms.s.hutchins@gmail.com>
2019-04-09 19:56:24 -0500

    Updated client.py

--- client.py
+++ client.py
@@ -6,8 +6,9 @@ import logging  # Look into proper logging with zeep
 import zeep
 
 david_wsdl_url = 'https://david-d.ncifcrf.gov/webservice/services/DAVIDWebService?wsdl'
-username = 'reviewer@reviewer.org'
+username = 'shutchins2@umc.edu'
 
 david_client = zeep.Client(wsdl=david_wsdl_url)
 
+# Add logging with zeep to show where authentication is trying to occur
 david_client.service.authenticate(username)
